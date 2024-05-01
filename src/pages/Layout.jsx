f9c4141228f2e1949982cc09bfab448d9477c4f0
chgoutham199 <chgoutham199@gmail.com>
2024-05-01 20:12:14 +0530

    Fix layout and complete add product styling

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,11 +6,11 @@ import Header from '../components/Header'
 export default function 
 () {
   return (
-    <div className='bg-black flex flex-row h-screen w-screen overflow-hidden float-left '>
+    <div className='bg-black flex flex-row h-screen w-screen overflow-y-auto '>
         <SideBar />
     <div className='flex-1 text-white'>
         <div className=''>  <Header />  </div>
-        <div className='p-3 '>{<Outlet/>}</div>
+        <div className='p-3'>{<Outlet/>}</div>
         </div>
     </div>
   )
